e7192f31cacb53204eaaccc5a5a8ca9241b59ccc
Haruki Yoshida <yoshidaharuki@Harukis-MacBook-Pro.local>
2025-01-20 00:32:51 +0900

    adding squares to the board

--- src/board.py
+++ src/board.py
@@ -1,4 +1,13 @@
+from square import Square
+
 class Board:
     def __init__(self):
-        self.squares = []
         self.goal = 100
+        
+        # Map the square location to each square. 
+        # If the square has no effect it will not be in the map.
+        self.squares = {}
+        
+    def add_special_square(self, location: int, square: Square) -> None:
+        if 1 <= location <= self.goal:
+            self.squares[location] = square
